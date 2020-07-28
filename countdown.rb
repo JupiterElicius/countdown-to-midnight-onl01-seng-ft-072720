65cec8fe8a0468a241693a94fa3fdddd80e62327
Nick Bazaldua <bazalnick@gmail.com>
2020-07-28 20:17:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,6 @@
 #write your code here
 def countdown
+  countdown = 10
   while countdown > 0 
   puts "#{countdown} SECOND(S)!"
   countdown -= 1
@@ -8,4 +9,3 @@ def countdown
 end
 
 def countdown_with_sleep
-  sleep(1)
